90767d2a6bebe7cd87d44f9f3499a3644c22879a
Vin Bertinelli <vinbertinelli@gmail.com>
2019-01-07 00:14:12 -0500

    include Common.s in stadium preload codes

--- Preload Stadium Transformations/Static Function - Get and Load Transformation.asm
+++ Preload Stadium Transformations/Static Function - Get and Load Transformation.asm
@@ -1,45 +1,6 @@
 #To be inserted at 80005600
-.macro branchl reg, address
-lis \reg, \address @h
-ori \reg,\reg,\address @l
-mtctr \reg
-bctrl
-.endm
+.include "../Common/Common.s"
 
-.macro branch reg, address
-lis \reg, \address @h
-ori \reg,\reg,\address @l
-mtctr \reg
-bctr
-.endm
-
-.macro load reg, address
-lis \reg, \address @h
-ori \reg, \reg, \address @l
-.endm
-
-.macro loadf regf,reg,address
-lis \reg, \address @h
-ori \reg, \reg, \address @l
-stw \reg,-0x4(sp)
-lfs \regf,-0x4(sp)
-.endm
-
-.macro backup
-mflr r0
-stw r0, 0x4(r1)
-stwu	r1,-0x100(r1)	# make space for 12 registers
-stmw  r20,0x8(r1)
-.endm
-
-.macro restore
-lmw  r20,0x8(r1)
-lwz r0, 0x104(r1)
-addi	r1,r1,0x100	# release the space
-mtlr r0
-.endm
-
-.set HSD_Randi,0x80380580
 .set PSData,31
 
 backup
