964c79bd239e7b8d85c48380f92c540f1fbcb20a
Vin Bertinelli <vinbertinelli@gmail.com>
2019-01-28 16:56:55 -0500

    remove stage load skip code + re-add dpad down loads random stage code

---
+++ External/DPad Down at CSS for Random Stage/DPad Down at CSS for Random Stage.asm
@@ -0,0 +1,23 @@
+#To be inserted at 8026325c
+.include "../../Common/Common.s"
+
+#Get Random Stage Byte
+load r4,0x8045BF17
+
+#Check for DPad Down
+  rlwinm.	r0, r7, 0, 29, 29
+  beq CheckForStart
+  li r3,0x1
+  stb r3,0x0(r4)
+  branch r12,0x80263264
+
+CheckForStart:
+#Check for Start
+  rlwinm.	r0, r7, 0, 19, 19
+  beq Exit
+  li r3,0x0
+  stb r3,0x0(r4)
+
+#Original Codeline
+Exit:
+  rlwinm.	r0, r7, 0, 19, 19
