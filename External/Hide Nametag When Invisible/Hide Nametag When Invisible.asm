60d4db05ab9f36ab4ac9e765c1f40c068f421ef5
Vin Bertinelli <vinbertinelli@gmail.com>
2019-02-15 18:38:33 -0500

    hide nametag when mewtwo airdodges

--- External/Hide Nametag When Invisible/Hide Nametag When Invisible.asm
+++ External/Hide Nametag When Invisible/Hide Nametag When Invisible.asm
@@ -10,11 +10,21 @@
   lbz r3, 0(r31)
   branchl r12,0x80034110
 #Get Player Data
-  lwz r3,0x2C(r3)
+  lwz r4,0x2C(r3)
+#Check if Mewtwo
+  lwz r3,0x4(r4)
+  cmpwi r3,0x10
+  bne NotMewtwoAridodge
+#Check if Airdodging
+  lwz r3,0x10(r4)
+  cmpwi r3,0xEC
+  beq HideNametag
+NotMewtwoAridodge:
 #Get Invis Bit
-  lbz r3,0x221E(r3)
+  lbz r3,0x221E(r4)
   rlwinm. r3, r3, 0, 24, 24
   beq- Original
+HideNametag:
 #Is Invisible, Hide Tag
   lis r12, 0x802F
   ori r12, r12, 0xCCC8
