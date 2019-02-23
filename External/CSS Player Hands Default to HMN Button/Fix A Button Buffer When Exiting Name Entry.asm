8d35296807f121248d7604248e7a393733e7fa2f
Vin Bertinelli <vinbertinelli@gmail.com>
2019-02-22 19:56:38 -0500

    fix name entry->CSS bug
    fixed bug where pressing A to confirm name entry would auto press the A button first frame of the CSS

---
+++ External/CSS Player Hands Default to HMN Button/Fix A Button Buffer When Exiting Name Entry.asm
@@ -0,0 +1,13 @@
+#To be inserted at 80261a6c
+#Credit Achilles1515
+.include "../../Common/Common.s"
+
+#Check
+  lbz	r5, 0x0005 (r31)
+  cmpwi	r5, 2
+  bne Exit
+#Skip Code
+  branch r12,0x80261B6C
+
+Exit:
+  mulli	r0, r19, 36
