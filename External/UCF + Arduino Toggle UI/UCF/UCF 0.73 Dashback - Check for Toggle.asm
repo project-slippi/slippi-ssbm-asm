39de1c1724feefb110729f6d0657326b7944b688
Jas Laferriere <Fizzi36@gmail.com>
2019-09-11 23:41:16 -0700

    add commented dashback code

--- External/UCF + Arduino Toggle UI/UCF/UCF 0.73 Dashback - Check for Toggle.asm
+++ External/UCF + Arduino Toggle UI/UCF/UCF 0.73 Dashback - Check for Toggle.asm
@@ -7,90 +7,116 @@
   subi r5,rtoc,ControllerFixOptions   #get UCF toggle bool base address
   lbzx r4,r6,r5	          #get players UCF toggle bool
   cmpwi r4,0x1
-  beq loc_0x0 # if equal, skip to start of handler
+  beq CHECK_IF_SLOW_TURN # if equal, skip to start of handler
 # Check for dashback bool (set by slp playback)
   subi r5,rtoc,DashbackOptions
   lbzx r4,r6,r5
   cmpwi r4,0x1
-  bne loc_0x108
+  bne END
 
-loc_0x0:
-  lhz r4, 1000(r31)
-  cmpwi r4, 0x4000
-  bne- loc_0x108
-  lwz r4, -20812(r13)
-  lfs f1, 1568(r31)
-  lfs f2, 9028(r31)
-  fmuls f1, f1, f2
-  lfs f2, 60(r4)
-  fcmpo cr0, f1, f2
-  cror 2, 1, 2
-  bne- loc_0x108
-  lbz r5, 1648(r31)
-  cmpwi r5, 0x2
-  bge- loc_0x108
-  lbz r4, 8735(r31)
-  rlwinm. r4, r4, 0, 28, 28
-  beq+ loc_0x48
-  b loc_0x108
+  CHECK_IF_SLOW_TURN:
+  lhz r4,0x3E8(r31)  # Check if 2nd frame of turn
+  cmpwi r4,0x4000
+  bne- END
 
-loc_0x48:
-  lis r4, 0x804C
-  ori r4, r4, 0x1F78
-  lbz r5, 1(r4)
-  stb r5, -8(r1)
-  b loc_0x94
+  CHECK_SMASH_TURN_INPUT_CONDITIONS:
 
-loc_0x5C:
-  subi r5, r5, 0x1
-  cmpwi r5, 0x0
-  bge- loc_0x6C
-  addi r5, r5, 0x5
+  lwz r4,-0x514C(r13)
+  lfs f1,0x0620(r31)
+  lfs f2,0x2344(r31)
+  fmuls f1,f1,f2
+  lfs f2,0x3C(r4)
+  fcmpo cr0,f1,f2     # Load control stick x-inputs and compare to 0.8
+  cror 2,1,2
+  bne- END
+  lbz r5,0x0670(r31)
+  cmpwi r5,2  # Check that cardinal direction not held 2+ frames
+  bge- END  # Skip if no dash input
 
-loc_0x6C:
-  lis r4, 0x8046
-  ori r4, r4, 0xB108
-  mulli r5, r5, 0x30
-  add r4, r4, r5
+  SKIP_IF_NANA:
+  lbz r4,0x221f(r31)    # load flags
+  rlwinm. r4,r4,0,28,28    # (08) = secondary character
+  beq+ BEGIN_HW_INPUTS
+  b END
+
+  BEGIN_HW_INPUTS:
+  lis r4, 0x804c
+  ori r4, r4, 0x1f78
+  lbz r5, 0x0001(r4)    # HSD_PadRenewMasterStatus gets index for which inputs to get from here
+  stb r5, -0x0008(sp)
+
+  b LOAD_2_FRAMES_PAST_INPUTS
+
+  FETCH_INPUT:   # Gets hw input according to controller port and frame index in r5
+
+  FIX_INDEX_AND_CHECK_IF_LESS_THAN_ZERO:
+  subi r5, r5, 1
+  cmpwi r5,0
+  bge- GET_INPUT
+
+  INDEX_IS_ZERO:
+  addi r5, r5, 5
+
+  GET_INPUT:
+  lis r4, 0x8046  # Input array location.
+  ori r4, r4, 0xb108
+  mulli r5, r5, 48
+  add r4, r4, r5  # Add index to get inputs from the right frame
   lbz r5, 0x618(r31)   # load controller port
   mulli r5, r5, 0xC
   add r4, r4, r5
-  lbz r5, 2(r4)
-  extsb r5, r5
+  lbz r5, 0x02(r4)   # load x-input
+  extsb r5, r5    # convert to 32-bit signed int
   blr
 
-loc_0x94:
-  subi r5, r5, 0x2
-  bl loc_0x5C
-  stw r5, -12(r1)
-  lbz r5, -8(r1)
-  bl loc_0x5C
-  lwz r4, -12(r1)
+  LOAD_2_FRAMES_PAST_INPUTS:
+  subi r5, r5, 2
+  bl FETCH_INPUT
+  stw r5, -0x000C(sp)
+
+  LOAD_CURRENT_FRAME_INPUT:
+  lbz r5, -0x0008(sp)
+  bl FETCH_INPUT
+
+  CALCULATE_DIFFERENCE:
+  lwz r4, -0x000C(sp) # Load 2 frames old x-input
   sub r5, r5, r4
-  mullw r5, r5, r5
-  cmpwi r5, 0x15F9
-  ble- loc_0x108
-  li r0, 0x1
-  stw r0, 9048(r31)
-  stw r0, 9024(r31)
-  lbz r4, 7(r31)
-  cmpwi r4, 0xA
-  bne+ loc_0x108
+  mullw r5, r5, r5  # Take square to get positive value for comparison
+
+  THRESHOLD_TEST:
+  cmpwi r5, 0x15F9   # compare square of input difference between current frame and 2 frames ago to square of 75
+  ble- END
+
+  CHANGE_TO_SMASH_TURN:
+  li r0, 1
+  stw r0, 0x2358(r31)
+  stw r0, 0x2340(r31)   # Change smash turn and can dash flags
+
+
+  CHECK_IF_POPO:
+  lwz  r4,0x4(r31)   # Load character id
+  cmpwi  r4, 0xA    # Check if Popo
+  bne+ END          # Skip if not Popo
+
+  #Get Nana's Block unsafely
   lwz r4, 16(r3)
   lwz r4, 44(r4)
-  lwz r4, 7884(r4)
-  stfs f0, 24(r4)
-  lwz r5, 24(r4)
-  lis r12, 0x3F80
-  cmpw r5, r12
-  beq- loc_0x100
-  li r5, 0x80
-  stb r5, 6(r4)
-  b loc_0x108
 
-loc_0x100:
-  li r5, 0x7F
-  stb r5, 6(r4)
+  STORE_INPUTS_FOR_NANA:
+  lwz r4,0x1ECC(r4) # Load address where popos inputs were last stored
+  stfs f0,0x18(r4)   # Store popos direction for nana
+  lwz r5,0x18(r4)    # Load the direction to get dash direction
+  lis r12,0x3F80
+  cmpw r5,r12  # Compare to rightward
 
-loc_0x108:
-  stfs f0, 44(r31)
+  beq- POPO_TURNED_RIGHT
+  li r5,0x80      # This part is run if Popos direction was leftward
+  stb r5,0x6(r4)  # Store leftward 1.0 input for Nana.
+  b END
+
+  POPO_TURNED_RIGHT:
+  li r5,0x7f
+  stb r5,0x6(r4) # Store rightward 1.0 input for Nana
+
+  END:
+  stfs f0, 0x002C(r31)    # Entry point, store new facing direction
