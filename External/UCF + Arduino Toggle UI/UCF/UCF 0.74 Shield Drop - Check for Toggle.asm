4062e22842a8e1d476d5267ba8454bea5bdece6f
Vin Bertinelli <vinbertinelli@gmail.com>
2019-10-01 03:54:14 -0400

    re-add floor for nanas inputs

--- External/UCF + Arduino Toggle UI/UCF/UCF 0.74 Shield Drop - Check for Toggle.asm
+++ External/UCF + Arduino Toggle UI/UCF/UCF 0.74 Shield Drop - Check for Toggle.asm
@@ -33,44 +33,21 @@ Start:
   fcmpo cr0, f1, f0
   ble- EnterSpotdodge
 
-#Check if left stick is ?
-  lfs f0, 0x620(REG_PlayerData)
-  fabs f0, f0
-#Multiply by 80
-  lfs f1,OFST_Unk1(REG_Floats)
-  fmuls f0, f0, f1
-#Subtract 0.0001
-  lfs f1,OFST_Unk2(REG_Floats)
-  fsubs f0, f0, f1
-#Add 2
-  lfs f1,OFST_2fp(REG_Floats)
-  fadds f0,f0,f1
-  lfs f1,OFST_Unk1(REG_Floats)
-  fdivs f0, f0, f1
-#Remember this value
-  fmr f3,f0
-#Check if left stick is ?
-  lfs f0, 0x624(REG_PlayerData)
-  fabs f0, f0
-#Multiply by 80
-  lfs f1,OFST_Unk1(REG_Floats)
-  fmuls f0, f0, f1
-#Subtract 0.0001
-  lfs f1,OFST_Unk2(REG_Floats)
-  fsubs f0, f0, f1
-#Add 2
-  lfs f1,OFST_2fp(REG_Floats)
-  fadds f0,f0,f1
-  lfs f1,OFST_Unk1(REG_Floats)
-  fdivs f0, f0, f1
+#Check if left stick X is ?
+  lfs f1, 0x620(REG_PlayerData)
+  bl  DoSomething
+  stfs  f1,0x90(sp)
+#Check if left stick Y is ?
+  lfs f1, 0x624(REG_PlayerData)
+  bl  DoSomething
+  lfs  f2,0x90(sp)
 
 #Check if stick was pushed slow enough?
-  fmr f1,f3
+  fmuls f2, f2, f2
   fmuls f1, f1, f1
-  fmuls f0, f0, f0
-  fadds f0, f0, f1
-  lfs f1,OFST_1fp(REG_Floats)
-  fcmpo cr0, f0, f1
+  fadds f1, f1, f2
+  lfs f2,OFST_1fp(REG_Floats)
+  fcmpo cr0, f1, f2
   blt EnterSpotdodge
 #Check if stick has been pushed horizontally for over 3 frames
   lbz r4, 0x670(REG_PlayerData)
@@ -94,20 +71,53 @@ Start:
 	mtlr	r3
 	blr
 
+#############################
+DoSomething:
+#f1 = input
+
+  fabs f0, f1
+#Multiply by 80
+  lfs f1,OFST_Unk1(REG_Floats)
+  fmuls f0, f0, f1
+#Subtract 0.0001
+  lfs f1,OFST_Unk2(REG_Floats)
+  fsubs f0, f0, f1
+#Floor value
+  fctiwz  f0,f0
+  stfd  f0,0x80(sp)
+  lwz r3,0x84(sp)
+#Add 2
+  addi  r3,r3,2
+#Cast back
+  lis	r0, 0x4330
+  lfd	f2, OFST_MagicNumber (REG_Floats)
+  xoris	r3, r3,0x8000
+  stw	r0,0x80(sp)
+  stw	r3,0x84(sp)
+  lfd	f1,0x80(sp)
+  fsubs	f0,f1,f2		#Convert To Float
+#Divide by 80
+  lfs f1,OFST_Unk1(REG_Floats)
+  fdivs f1, f0, f1
+#Exit
+  blr
+#############################
+
 Floats:
 blrl
 .set  OFST_Unk1,0x0
 .set  OFST_Unk2,0x4
 .set  OFST_Unk3,0x8
-.set  OFST_2fp,0xC
-.set  OFST_1fp,0x10
-.set  OFST_Unk4,0x14
+.set  OFST_1fp,0xC
+.set  OFST_Unk4,0x10
+.set  OFST_MagicNumber,0x14
 .float 80
 .long 0x37270000
 .float 176
-.float 2
 .float 1
 .float -0.8
+.long 0x43300000
+.long 0x80000000
 
 EnterSpotdodge:
   mr	r3, REG_PlayerGObj
