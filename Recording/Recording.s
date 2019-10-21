8a58ec265510834b298a86c1645887e0dcf10960
Jas Laferriere <Fizzi36@gmail.com>
2019-10-21 14:45:54 -0700

    better define buffer size

--- Recording/Recording.s
+++ Recording/Recording.s
@@ -8,7 +8,17 @@
 .set GAME_POST_FRAME_PAYLOAD_LENGTH, 52 # byte count
 .set GAME_ITEM_INFO_PAYLOAD_LENGTH, 31 # byte count
 .set GAME_END_PAYLOAD_LENGTH, 2 # byte count
-.set FULL_FRAME_DATA_BUF_LENGTH, 8 * (GAME_PRE_FRAME_PAYLOAD_LENGTH + 1) + 8 * (GAME_POST_FRAME_PAYLOAD_LENGTH + 1) + ((GAME_ITEM_INFO_PAYLOAD_LENGTH + 1) * MAX_ITEMS)
+
+# Calculate out the maximum buffer length that will be needed. This buffer
+# is also used for transferring message descriptions and game info but that
+# length should be less than the frame buf length
+.set SUPPORTED_PORTS, 4
+.set MAX_CHARACTERS, SUPPORTED_PORTS * 2
+.set TOTAL_INITIAL_RNG_LEN, GAME_INITIAL_RNG_PAYLOAD_LENGTH + 1
+.set TOTAL_CHAR_FRAME_LEN, MAX_CHARACTERS * (GAME_PRE_FRAME_PAYLOAD_LENGTH + 1) + MAX_CHARACTERS * (GAME_POST_FRAME_PAYLOAD_LENGTH + 1)
+.set TOTAL_ITEM_LEN, MAX_ITEMS * (GAME_ITEM_INFO_PAYLOAD_LENGTH + 1)
+.set TOTAL_GAME_END_LEN, GAME_END_PAYLOAD_LENGTH + 1
+.set FULL_FRAME_DATA_BUF_LENGTH, TOTAL_INITIAL_RNG_LEN + TOTAL_CHAR_FRAME_LEN + TOTAL_ITEM_LEN + TOTAL_GAME_END_LEN
 
 # build version number. Each byte is one digit
 # any change in command data should result in a minor version change
