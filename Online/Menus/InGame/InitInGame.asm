a46b8101ef459ab81e3382e467447038001a36f1
Vin Bertinelli <vinbertinelli@gmail.com>
2020-06-30 22:21:00 -0400

    left align tags

--- Online/Menus/InGame/InitInGame.asm
+++ Online/Menus/InGame/InitInGame.asm
@@ -56,7 +56,7 @@ blrl
 .set DOFST_PLAYERBG_COLOR, DOFST_PLAYERBG_OPA + 4
 .byte 0,0,0,255
 .set DOFST_PLAYERBG_SCALEBASE, DOFST_PLAYERBG_COLOR + 4
-.float 12
+.float 23
 .set DOFST_PLAYERBG_SCALEMULT, DOFST_PLAYERBG_SCALEBASE + 4
 .float 2.3
 .set DOFST_PLAYERBG_YSCALE, DOFST_PLAYERBG_SCALEMULT + 4
@@ -69,13 +69,41 @@ blrl
 .set DOFST_PLAYERBG_XSCALEMULT, DOFST_PLAYERBG_XSCALEBASE + 4
 .float 0.25
 
+.set DOFST_PLAYERTEXT_XPOS, DOFST_PLAYERBG_XSCALEMULT + 4
+.float 274
+.set DOFST_PLAYERTEXT_YPOS, DOFST_PLAYERTEXT_XPOS + 4
+.float 452
+.set DOFST_PLAYERTEXT_ZPOS, DOFST_PLAYERTEXT_YPOS + 4
+.float 0
+.set DOFST_PLAYERTEXT_WIDTH, DOFST_PLAYERTEXT_ZPOS + 4
+.float 110
+.set DOFST_PLAYERTEXT_HEIGHT, DOFST_PLAYERTEXT_WIDTH + 4
+.float 300
+.set DOFST_PLAYERTEXT_CANVASSCALE, DOFST_PLAYERTEXT_HEIGHT + 4
+.float 1 #0.0521
 
 # strings
-.set DOFST_TEXT_FORMATSTRING, DOFST_PLAYERBG_XSCALEMULT + 4
+.set DOFST_TEXT_FORMATSTRING, DOFST_PLAYERTEXT_CANVASSCALE + 4
 .string "%s"
 .align 2
 .set DOFST_TEXT_DELAYSTRING, DOFST_TEXT_FORMATSTRING + 4
 .string "Delay: %df"
+.align 2
+
+.set DOFST_TEXTHEADER_SIZE, 10
+TEXT_HEADER_BLRL:
+blrl
+#.byte 0x10                  #center
+.byte 0x16                  #kerning
+.byte 0xC, 255, 255, 255    #color
+.byte 0x0E
+.hword 138, 138             #bound
+.align 2
+
+.set DOFST_TEXTTERMINATOR_SIZE, 3
+TEXT_TERMINATOR_BLRL:
+blrl
+.byte 0xf, 0xd, 00
 .align 2
 
 CODE_START:
@@ -142,34 +170,14 @@ branchl r12, Text_UpdateSubtextSize
 ## Display Player Names ##
 ##########################
 
-# Start prepping text struct
-li r3, 2
-lwz r4, -0x4924 (r13) # Same canvas as nametags
-branchl r12, Text_CreateStruct
-mr REG_TEXT_STRUCT, r3
-
-# Set text kerning to close
-li r4, 0x1
-stb r4, 0x49(REG_TEXT_STRUCT)
-# Set text to align center
-li r4, 0x1
-stb r4, 0x4A(REG_TEXT_STRUCT)
-
-# Store Base Z Offset
-lfs f1, DOFST_TEXT_BASE_Z(REG_DATA_ADDR) #Z offset
-stfs f1, 0x8(REG_TEXT_STRUCT)
-
-# Scale Canvas Down
-lfs f1, DOFST_TEXT_BASE_CANVAS_SCALING(REG_DATA_ADDR)
-stfs f1, 0x24(REG_TEXT_STRUCT)
-stfs f1, 0x28(REG_TEXT_STRUCT)
-
 # Display all player names
 .set REG_COUNT, 20
+.set REG_TAG_BUFFER, 21
+.set REG_TAG_SIZE, 22
+.set REG_TAG_ALLOC, 23
 .set REG_HUDPOS, 21
 .set REG_BG_JOBJ, 22
-.set REG_SUBTEXT, 23
-.set REG_BG_GOBJ, 24
+.set REG_BG_GOBJ, 23
 li  REG_COUNT, 0
 load REG_HUDPOS, 0x804a0ff0
 DISPLAY_NAME_LOOP:
@@ -179,33 +187,112 @@ branchl r12, 0x8003241c
 cmpwi r3, 3
 beq DISPLAY_NAME_INC_LOOP
 
-#Create gobj
-li  r3,14
-li  r4,15
-li  r5,0
-branchl r12,0x803901f0
-mr  REG_BG_GOBJ,r3
-#Create Background
-load  r3,0x804a1ed0
-lwz r3,0x0(r3)
-branchl r12,0x80370e44
-mr  REG_BG_JOBJ,r3
-# Add as object
-mr  r3,REG_BG_GOBJ
-lbz	r4, -0x3E57 (r13)
-mr  r5,REG_BG_JOBJ
-branchl r12,0x80390a70
-# Add GX Link
-mr  r3,REG_BG_GOBJ
-load  r4,0x80391070
-li  r5,9
-li  r6,0
-branchl r12,0x8039069c
+# Calculate X Position
+#Get HUD Position
+mr  r3,REG_COUNT
+branchl r12,0x802f3424
+# Set text
+lfs f1, 0x0(r3)
+lfs f2,DOFST_HUDPOS_MULT(REG_DATA_ADDR)
+# Scale Text X Position based on Aspect Ratio
+lfs  f3,0x7C(sp)
+fmuls f2,f2,f3
+fmuls f1,f1,f2
+lfs f2,DOFST_PLAYERTEXT_XPOS(REG_DATA_ADDR)
+fadds f1,f1,f2
+#lfs f1, DOFST_PLAYERTEXT_XPOS (REG_DATA_ADDR)
+stfs f1, 0x70 (sp)
+
+# Start prepping text struct
+li r3, 2
+lwz r4, -0x4924 (r13) # Same canvas as nametags
+lfs f1, 0x70 (sp)
+lfs f2, DOFST_PLAYERTEXT_YPOS (REG_DATA_ADDR)
+lfs f3, DOFST_PLAYERTEXT_ZPOS (REG_DATA_ADDR)
+lfs f4, DOFST_PLAYERTEXT_WIDTH (REG_DATA_ADDR)
+lfs f5, DOFST_PLAYERTEXT_HEIGHT (REG_DATA_ADDR)
+branchl r12, 0x803a5acc
+mr REG_TEXT_STRUCT, r3
+
+# Fixed Width
+li r4, 0x1
+stb r4, 0x48(REG_TEXT_STRUCT)
+# Set text to align center
+li r4, 0x0
+stb r4, 0x4A(REG_TEXT_STRUCT)
+
+# Scale Canvas Down
+lfs f1, DOFST_PLAYERTEXT_CANVASSCALE(REG_DATA_ADDR)
+stfs f1, 0x24(REG_TEXT_STRUCT)
+stfs f1, 0x28(REG_TEXT_STRUCT)
 
 #############################
 ## Create Player Name Text ##
 #############################
 
+# alloc buffer of size (15 * 3) + 1. (3 bytes per character, plus a terminator)
+li  r3,(15*3) + 1
+branchl r12,HSD_MemAlloc
+mr  REG_TAG_BUFFER,r3
+
+# convert to menu text
+mr  r3,REG_TAG_BUFFER
+addi r4, REG_MSRB_ADDR, MSRB_P1_NAME
+mulli r5, REG_COUNT, 31
+add r4,r4,r5
+branchl r12,0x803a67ec
+mr  REG_TAG_SIZE,r3
+
+# alloc mem for menu text
+addi  r3,REG_TAG_SIZE, DOFST_TEXTHEADER_SIZE + DOFST_TEXTTERMINATOR_SIZE
+branchl r12,0x803a5798
+mr  REG_TAG_ALLOC,r3
+
+# copy header
+mr  r3,REG_TAG_ALLOC
+bl  TEXT_HEADER_BLRL
+mflr  r4
+li  r5,DOFST_TEXTHEADER_SIZE
+branchl r12,memcpy
+
+# copy text
+addi  r3, REG_TAG_ALLOC, DOFST_TEXTHEADER_SIZE
+mr  r4, REG_TAG_BUFFER
+mr  r5,REG_TAG_SIZE
+branchl r12,memcpy
+
+# copy terminator
+addi  r3, REG_TAG_ALLOC, DOFST_TEXTHEADER_SIZE
+add r3,r3,REG_TAG_SIZE
+bl  TEXT_TERMINATOR_BLRL
+mflr  r4
+li  r5,DOFST_TEXTTERMINATOR_SIZE
+branchl r12,memcpy
+
+mr  r3,REG_TEXT_STRUCT
+li  r4,0
+branchl r12,0x803a6368
+stw REG_TAG_ALLOC,0x5C(REG_TEXT_STRUCT)
+/*
+# finalize struct
+stw REG_TAG_ALLOC,0x5C(REG_TEXT_STRUCT)
+li  r3,0
+stw r3, 0x60(REG_TEXT_STRUCT)                  #store 0 to curr (0x60)
+li  r3,16
+branchl r12,0x803a5798
+stw r3,0x68(REG_TEXT_STRUCT)                #store size
+li  r4,16
+branchl r12, Zero_AreaLength                #zero out
+li  r3,16
+sth r3,0x6E(REG_TEXT_STRUCT)                #store size
+*/
+
+# free buffer
+mr  r3, REG_TAG_BUFFER
+branchl r12,HSD_MemAlloc
+
+
+/*
 #Get HUD Position
 mulli r3,REG_COUNT,0xC
 add r3,r3,REG_HUDPOS
@@ -234,11 +321,35 @@ mr r3, REG_TEXT_STRUCT
 mr  r4,REG_SUBTEXT
 lfs f2, DOFST_PLAYERTEXT_SIZE(REG_DATA_ADDR)
 branchl r12, Text_UpdateSubtextSize
-
+*/
 
 ############################
 ## Create Text Background ##
 ############################
+
+
+#Create gobj
+li  r3,14
+li  r4,15
+li  r5,0
+branchl r12,0x803901f0
+mr  REG_BG_GOBJ,r3
+#Create Background
+load  r3,0x804a1ed0
+lwz r3,0x0(r3)
+branchl r12,0x80370e44
+mr  REG_BG_JOBJ,r3
+# Add as object
+mr  r3,REG_BG_GOBJ
+lbz	r4, -0x3E57 (r13)
+mr  r5,REG_BG_JOBJ
+branchl r12,0x80390a70
+# Add GX Link
+mr  r3,REG_BG_GOBJ
+load  r4,0x80391070
+li  r5,9
+li  r6,0
+branchl r12,0x8039069c
 
 # Get HUD pos
 mr  r3,REG_COUNT
@@ -285,6 +396,7 @@ lwz r4, DOFST_PLAYERBG_COLOR (REG_DATA_ADDR)
 stw r4, 0x4(r3)
 
 
+/*
 # Get total width of characters used in tag
 .set  CHAR_WIDTH_MAX, 17
 .set  TAG_WIDTH_MIN, 50
@@ -295,9 +407,7 @@ stw r4, 0x4(r3)
 li  REG_WIDTH,0
 # Get subtext contents
 lwz  r3,0x5C(REG_TEXT_STRUCT)
-mr  r4,REG_SUBTEXT
-branchl r12,0x803a6fec
-addi  REG_CURR, r3, 0xE   #skip past header
+addi  REG_CURR, r3, DOFST_TEXTHEADER_SIZE   #skip past header
 load REG_WIDTH_INTERNAL, 0x8040cb00
 lbz r3,0x4F(REG_TEXT_STRUCT)
 mulli r3,r3,4
@@ -361,7 +471,11 @@ fsubs	f1,f1,f2		#Convert To Float
 lfs f2, DOFST_PLAYERBG_XSCALEMULT (REG_DATA_ADDR)
 fmuls f1,f1,f2
 stfs f1, 0x2C (REG_BG_JOBJ)
+*/
 
+# Set bg width
+lfs f1, DOFST_PLAYERBG_SCALEBASE (REG_DATA_ADDR)
+stfs f1, 0x2C (REG_BG_JOBJ)
 # Scale BG X based on Aspect Ratio
 lfs  f1,0x7C(sp)
 lfs f2, 0x2C (REG_BG_JOBJ)
