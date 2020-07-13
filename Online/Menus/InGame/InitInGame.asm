68e31a4bb387c90b13bd8cee183597470fa956b4
Vin Bertinelli <vinbertinelli@gmail.com>
2020-07-13 04:54:41 -0400

    tag display: use HUD cobj

--- Online/Menus/InGame/InitInGame.asm
+++ Online/Menus/InGame/InitInGame.asm
@@ -5,12 +5,6 @@
 .include "Common/Common.s"
 .include "Online/Online.s"
 
-.set REG_ODB_ADDRESS, 31
-.set REG_TEXT_STRUCT, 30
-.set REG_DATA_ADDR, 29
-.set REG_STRING_BUF, 28
-.set REG_MSRB_ADDR,27
-
 # Ensure that this is an online in-game
 getMinorMajor r3
 cmpwi r3, SCENE_ONLINE_IN_GAME
@@ -29,50 +23,33 @@ blrl
 
 # delay values
 .set DOFST_TEXT_X_POS, DOFST_TEXT_BASE_CANVAS_SCALING + 4
-.float 616
+.float 27
 .set DOFST_TEXT_Y_POS, DOFST_TEXT_X_POS + 4
-.float 435
+.float -10
 .set DOFST_TEXT_SIZE, DOFST_TEXT_Y_POS + 4
-.float 0.41
+.float 0.04
 
-# player text values
-.set DOFST_PLAYERTEXT_X_POS, DOFST_TEXT_SIZE + 4
-.float 605
-.set DOFST_PLAYERTEXT_Y_POS, DOFST_PLAYERTEXT_X_POS + 4
-.float 437
-.set DOFST_PLAYERTEXT_SIZE, DOFST_PLAYERTEXT_Y_POS + 4
-.float 0.5
-
-# player text center calculation (HUDX * 11) + 330
-.set DOFST_HUDPOS_MULT, DOFST_PLAYERTEXT_SIZE + 4
-.float 11
-.set DOFST_HUDPOS_OFFSET, DOFST_HUDPOS_MULT + 4
-.float 330
 
 
 # BG values
-.set DOFST_PLAYERBG_OPA, DOFST_HUDPOS_OFFSET + 4
+.set DOFST_PLAYERBG_OPA, DOFST_TEXT_SIZE + 4
 .float 0.33
 .set DOFST_PLAYERBG_COLOR, DOFST_PLAYERBG_OPA + 4
 .byte 0,0,0,255
-.set DOFST_PLAYERBG_SCALEBASE, DOFST_PLAYERBG_COLOR + 4
-.float 23
-.set DOFST_PLAYERBG_SCALEMULT, DOFST_PLAYERBG_SCALEBASE + 4
-.float 2.3
-.set DOFST_PLAYERBG_YSCALE, DOFST_PLAYERBG_SCALEMULT + 4
-.float 8
-.set DOFST_PLAYERBG_YOFST, DOFST_PLAYERBG_YSCALE + 4
-.float -486.5
-# BG scale per letter
-.set DOFST_PLAYERBG_XSCALEBASE, DOFST_PLAYERBG_YOFST + 4
-.float 0.4
-.set DOFST_PLAYERBG_XSCALEMULT, DOFST_PLAYERBG_XSCALEBASE + 4
-.float 0.2
+.set DOFST_PLAYERBG_YSCALE, DOFST_PLAYERBG_COLOR + 4
+.float 0.8
+.set DOFST_PLAYERBG_XOFST, DOFST_PLAYERBG_YSCALE + 4
+.float 0.775
+.set DOFST_PLAYERBG_YOFST, DOFST_PLAYERBG_XOFST + 4
+.float -24.5 #(DOFST_PLAYERTEXT_YPOS * 1.13953488372)
+# BG X scale per letter
+.set DOFST_PLAYERBG_XSCALEMULT, DOFST_PLAYERBG_YOFST + 4
+.float 0.014
 
 .set DOFST_PLAYERTEXT_XPOS, DOFST_PLAYERBG_XSCALEMULT + 4
-.float 276
+.float -3    #higher values = right
 .set DOFST_PLAYERTEXT_YPOS, DOFST_PLAYERTEXT_XPOS + 4
-.float 454
+.float 21.5     #higher values = down
 .set DOFST_PLAYERTEXT_ZPOS, DOFST_PLAYERTEXT_YPOS + 4
 .float 0
 .set DOFST_PLAYERTEXT_WIDTH, DOFST_PLAYERTEXT_ZPOS + 4
@@ -80,12 +57,10 @@ blrl
 .set DOFST_PLAYERTEXT_HEIGHT, DOFST_PLAYERTEXT_WIDTH + 4
 .float 300
 .set DOFST_PLAYERTEXT_CANVASSCALE, DOFST_PLAYERTEXT_HEIGHT + 4
-.float 0.75 #0.0521
-.set DOFST_PLAYERTEXT_XPOSWIDTHSCALE, DOFST_PLAYERTEXT_CANVASSCALE + 4
-.float 55 #0.0521
+.float 0.0521 #0.0521
 
 # strings
-.set DOFST_TEXT_DELAYSTRING, DOFST_PLAYERTEXT_XPOSWIDTHSCALE + 4
+.set DOFST_TEXT_DELAYSTRING, DOFST_PLAYERTEXT_CANVASSCALE + 4
 .string "Delay: %df"
 .align 2
 
@@ -105,20 +80,102 @@ blrl
 .byte 0xf, 0xd, 00
 .align 2
 
+#########################################
+COBJ_CB:
+blrl
+.set  REG_GOBJ,31
+
+backup
+
+mr  REG_GOBJ, r3
+
+/*
+# Check if paused
+li  r3,1
+branchl r12,0x801a45e8
+cmpwi r3,2
+beq COBJ_CB_Exit
+*/
+# Check if paused
+lbz	r0, -0x4934 (r13)
+cmpwi r0,1
+beq COBJ_CB_Exit
+
+# Draw camera
+mr  r3, REG_GOBJ
+branchl r12,0x803910d8
+
+COBJ_CB_Exit:
+restore
+blr
+#########################################
+
 CODE_START:
 backup
+
+# CObj stuff
+.set  COBJ_GXPRI, 8
+.set  TEXT_GXPRI, 80
+.set  TEXT_GXLINK, 12
+
+.set  REG_Canvas,31
+.set  REG_COBJ,30
+.set  REG_GOBJ,29
+
+# Get HUD CObjDesc
+load  r3, 0x804d6d5c
+lwz r3, 0x0 (r3)
+load  r4, 0x803f94d0
+branchl r12,0x80380358
+# Create CObj
+lwz r3,0x4(r3)
+lwz r3,0x0(r3)
+branchl r12,0x8036a590
+mr  REG_COBJ,r3
+# Create GObj
+li  r3,19
+li  r4,20
+li  r5,0
+branchl r12,0x803901f0
+mr  REG_GOBJ,r3
+# Add object
+mr  r3,REG_GOBJ
+lbz r4,-0x3E55(r13)
+mr  r5,REG_COBJ
+branchl r12,0x80390a70
+# Init camera
+mr  r3,REG_GOBJ
+bl  COBJ_CB
+mflr  r4
+li  r5, COBJ_GXPRI
+branchl r12,0x8039075c
+# Store COBJs GXLinks
+load  r3, 1 << TEXT_GXLINK
+stw r3, 0x24 (REG_GOBJ)
+
+# Create canvas
+li  r3,2
+mr  r4,REG_GOBJ
+li  r5, 9
+li  r6, 13
+li  r7, 0
+li  r8, TEXT_GXLINK
+li  r9, TEXT_GXPRI
+li  r10, COBJ_GXPRI
+branchl r12, 0x803a611c
+mr  REG_Canvas, r3
+
+
+.set REG_ODB_ADDRESS, 30
+.set REG_TEXT_STRUCT, 29
+.set REG_DATA_ADDR, 28
+.set REG_STRING_BUF, 27
+.set REG_MSRB_ADDR,26
 
 lwz REG_ODB_ADDRESS, OFST_R13_ODB_ADDR(r13) # data buffer address
 
 bl DATA_BLRL
 mflr REG_DATA_ADDR
-
-# Aspect Scalar
-lfs f3,DOFST_PLAYERBG_XSCALEBASE (REG_DATA_ADDR)
-load  r3,0x804ddb84
-lfs f2,0x0(r3)
-fdivs f1,f2,f3
-stfs  f1,0x7C(sp)
 
 # Get player names
 li r3, 0
@@ -127,7 +184,7 @@ mr REG_MSRB_ADDR, r3
 
 # Start prepping text struct
 li r3, 2
-lwz r4, -0x4924 (r13) # Same canvas as nametags
+mr  r4,REG_Canvas
 branchl r12, Text_CreateStruct
 mr REG_TEXT_STRUCT, r3
 
@@ -159,9 +216,7 @@ branchl r12, Text_InitializeSubtext
 mr r3, REG_TEXT_STRUCT
 li r4, 0
 # Scale text X based on Aspect Ratio
-lfs  f2,0x7C(sp)
 lfs f1, DOFST_TEXT_SIZE(REG_DATA_ADDR)
-fmuls f1,f1,f2
 lfs f2, DOFST_TEXT_SIZE(REG_DATA_ADDR)
 branchl r12, Text_UpdateSubtextSize
 
@@ -190,33 +245,20 @@ beq DISPLAY_NAME_INC_LOOP
 #Get HUD Position
 mr  r3,REG_COUNT
 branchl r12,0x802f3424
-# Set text
+# HUD X
 lfs f1, 0x0(r3)
-lfs f2,DOFST_HUDPOS_MULT(REG_DATA_ADDR)
-# Scale Text X Position based on Aspect Ratio
-lfs  f3,0x7C(sp)
-fmuls f2,f2,f3
-fmuls f1,f1,f2
-lfs f2,DOFST_PLAYERTEXT_XPOS(REG_DATA_ADDR)
-fadds f1,f1,f2
-# Scale Text X AGAIN because of stupid width
-lfs f3,DOFST_PLAYERTEXT_XPOSWIDTHSCALE(REG_DATA_ADDR)
-lfs  f2,0x7C(sp)
-fmuls f2,f2,f3
-fsubs f2,f3,f2
-fadds f1,f1,f2
 stfs f1, 0x70 (sp)
 
 
 # Start prepping text struct
 li r3, 2
-lwz r4, -0x4924 (r13) # Same canvas as nametags
+mr  r4, REG_Canvas
 lfs f1, 0x70 (sp)
+lfs f2, DOFST_PLAYERTEXT_XPOS (REG_DATA_ADDR)
+fadds f1,f1,f2
 lfs f2, DOFST_PLAYERTEXT_YPOS (REG_DATA_ADDR)
 lfs f3, DOFST_PLAYERTEXT_ZPOS (REG_DATA_ADDR)
 lfs f4, DOFST_PLAYERTEXT_WIDTH (REG_DATA_ADDR)
-lfs f5, 0x7C(sp)
-fmuls f4,f4,f5
 lfs f5, DOFST_PLAYERTEXT_HEIGHT (REG_DATA_ADDR)
 branchl r12, 0x803a5acc
 mr REG_TEXT_STRUCT, r3
@@ -298,42 +340,9 @@ sth r3,0x6E(REG_TEXT_STRUCT)                #store size
 mr  r3, REG_TAG_BUFFER
 branchl r12,HSD_MemAlloc
 
-
-/*
-#Get HUD Position
-mulli r3,REG_COUNT,0xC
-add r3,r3,REG_HUDPOS
-# Set text
-lfs f1, 0x0(r3)
-lfs f2,DOFST_HUDPOS_MULT(REG_DATA_ADDR)
-# Scale Text X Position based on Aspect Ratio
-lfs  f3,0x7C(sp)
-fmuls f2,f2,f3
-fmuls f1,f1,f2
-lfs f2,DOFST_HUDPOS_OFFSET(REG_DATA_ADDR)
-fadds f1,f1,f2
-lfs f2, DOFST_PLAYERTEXT_Y_POS(REG_DATA_ADDR)
-mr r3, REG_TEXT_STRUCT
-addi r4, REG_MSRB_ADDR, MSRB_P1_NAME
-mulli r5, REG_COUNT, 31
-add r4,r4,r5
-branchl r12, Text_InitializeSubtext
-mr  REG_SUBTEXT,r3
-# Scale text X based on Aspect Ratio
-lfs  f1,0x7C(sp)
-lfs f2, DOFST_PLAYERTEXT_SIZE(REG_DATA_ADDR)
-fmuls f1,f1,f2
-# Set size
-mr r3, REG_TEXT_STRUCT
-mr  r4,REG_SUBTEXT
-lfs f2, DOFST_PLAYERTEXT_SIZE(REG_DATA_ADDR)
-branchl r12, Text_UpdateSubtextSize
-*/
-
 ############################
 ## Create Text Background ##
 ############################
-
 
 #Create gobj
 li  r3,14
@@ -354,7 +363,7 @@ branchl r12,0x80390a70
 # Add GX Link
 mr  r3,REG_BG_GOBJ
 load  r4,0x80391070
-li  r5,9
+li  r5, TEXT_GXLINK
 li  r6,0
 branchl r12,0x8039069c
 
@@ -363,12 +372,7 @@ mr  r3,REG_COUNT
 branchl r12,0x802f3424
 # Set bg position
 lfs f1,0x0 (r3)
-lfs f2, DOFST_HUDPOS_MULT (REG_DATA_ADDR)
-# Scale BG X Position based on Aspect Ratio
-lfs  f3,0x7C(sp)
-fmuls f2,f2,f3
-fmuls f1,f1,f2
-lfs f2, DOFST_HUDPOS_OFFSET (REG_DATA_ADDR)
+lfs f2, DOFST_PLAYERBG_XOFST (REG_DATA_ADDR)
 fadds f1,f1,f2
 stfs  f1,0x38 (REG_BG_JOBJ)
 lfs f1, DOFST_PLAYERBG_YOFST (REG_DATA_ADDR)
@@ -376,13 +380,17 @@ stfs  f1,0x3C (REG_BG_JOBJ)
 # Adjust scale
 lfs f1, DOFST_PLAYERBG_YSCALE (REG_DATA_ADDR)
 stfs f1, 0x30 (REG_BG_JOBJ)
-# Remove unneccessary dobjs
+# Get JOBJ 1
 mr  r3,REG_BG_JOBJ
 addi  r4,sp,0x80
 li  r5,1
 li  r6,-1
 branchl r12,0x80011e24
-# Add as child to Percent JOBJ
+# Z transform = 0
+lwz r3,0x80(sp)
+li  r4,0
+stw r4,0x40(r3)
+# Remove unneccessary dobjs
 lwz r3,0x80(sp)
 lwz r3,0x18(r3)  #first dobj
 lwz r4,0x14(r3)
@@ -402,13 +410,12 @@ stfs f1, 0xC(r3)
 lwz r4, DOFST_PLAYERBG_COLOR (REG_DATA_ADDR)
 stw r4, 0x4(r3)
 
-
 # Get total width of characters used in tag
 .set  CHAR_WIDTH_MAX, 20
 .set  TAG_WIDTH_MIN, 60
 .set  TAG_WIDTH_MAX, 115
 .set  REG_WIDTH, 25
-.set  REG_CURR, 26
+.set  REG_CURR, 10
 .set  REG_WIDTH_INTERNAL, 12
 .set  REG_WIDTH_EXTERNAL, 11
 li  REG_WIDTH,0
@@ -482,22 +489,10 @@ lfs f2, DOFST_PLAYERBG_XSCALEMULT (REG_DATA_ADDR)
 fmuls f1,f1,f2
 stfs f1, 0x2C (REG_BG_JOBJ)
 
-
-# Set bg width
-#lfs f1, DOFST_PLAYERBG_SCALEBASE (REG_DATA_ADDR)
-#stfs f1, 0x2C (REG_BG_JOBJ)
-
-# Scale BG X based on Aspect Ratio
-lfs  f1,0x7C(sp)
-lfs f2, 0x2C (REG_BG_JOBJ)
-fmuls f1,f1,f2
-stfs f1, 0x2C (REG_BG_JOBJ)
-
 DISPLAY_NAME_INC_LOOP:
 addi REG_COUNT, REG_COUNT, 1
 cmpwi REG_COUNT, 6
 blt DISPLAY_NAME_LOOP
-
 
 # Free buffer
 mr  r3,REG_MSRB_ADDR
