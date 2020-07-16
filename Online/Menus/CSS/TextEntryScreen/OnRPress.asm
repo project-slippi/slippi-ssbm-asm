8c6ecf964c716d0e984baa127206b02804d52266
Charlie Miller <charliea.miller@gmail.com>
2020-07-16 11:04:35 -0800

    Added constants for DMA write behavior. Triggers now send an index value

--- Online/Menus/CSS/TextEntryScreen/OnRPress.asm
+++ Online/Menus/CSS/TextEntryScreen/OnRPress.asm
@@ -6,6 +6,7 @@
 .include "Online/Online.s"
 
 .set REG_CODE_INDEX, 21 
+.set REG_TX_ADDR, 27
 
 addi REG_CODE_INDEX, REG_CODE_INDEX, 1
 backup
@@ -18,9 +19,17 @@ li	r3, 3
 branchl r12, SFX_Menu_CommonSound
 
 mr r5, REG_CODE_INDEX
+logf LOG_LEVEL_NOTICE, "Pressed R. Current Index: %d" 
+
+li r3, CONST_SlippiCmdSendNameEntryIndex
+stb r3, NEDC_CMD(REG_TX_ADDR)
+stb REG_CODE_INDEX, NEDC_NAME_ENTRY_INDEX(REG_TX_ADDR)
+
+mr r3, REG_TX_ADDR 
+li r4, NEDC_SIZE
+li r5, CONST_ExiWrite
+branchl r12, FN_EXITransferBuffer
 
 restore
-
-logf LOG_LEVEL_NOTICE, "Pressed R. Current Index: %d" 
 
 branchl r12, 0x8023ce38
