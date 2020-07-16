cca81bbb9f68dd930963a7017cb167a4ad40c1c9
Charlie Miller <charliea.miller@gmail.com>
2020-07-15 23:35:56 -0800

    L and R presses now change an index variable and log it to console.

--- Online/Menus/CSS/TextEntryScreen/OnLPress.asm
+++ Online/Menus/CSS/TextEntryScreen/OnLPress.asm
@@ -5,14 +5,30 @@
 .include "Common/Common.s"
 .include "Online/Online.s"
 
+# TODO: Do more testing to make sure register isn't used elsewhere.
+.set REG_CODE_INDEX, 21 
+
+# Only decrement while index is > 0
+cmpwi REG_CODE_INDEX, 0x0
+ble EXIT 
+
+# Subtract direct codes index by 1
+subi REG_CODE_INDEX, REG_CODE_INDEX, 1
+
+# Stop all pending sounds
+branchl r12, SFX_StopSFXInstance
+
 backup
 
 # Play failure sound
 li	r3, 3
 branchl r12, SFX_Menu_CommonSound
 
+mr r5, REG_CODE_INDEX
+logf LOG_LEVEL_NOTICE, "Pressed L. Current Index: %d" 
+
 restore
 
-logf LOG_LEVEL_NOTICE, "Pressed L"
+EXIT:
 
 branchl r12, 0x8023ce38
--- Online/Menus/CSS/TextEntryScreen/OnRPress.asm
+++ Online/Menus/CSS/TextEntryScreen/OnRPress.asm
@@ -5,14 +5,22 @@
 .include "Common/Common.s"
 .include "Online/Online.s"
 
+.set REG_CODE_INDEX, 21 
+
+addi REG_CODE_INDEX, REG_CODE_INDEX, 1
 backup
+
+# Stop all pending sounds
+branchl r12, SFX_StopSFXInstance
 
 # Play failure sound
 li	r3, 3
 branchl r12, SFX_Menu_CommonSound
 
+mr r5, REG_CODE_INDEX
+
 restore
 
-logf LOG_LEVEL_NOTICE, "Pressed R"
+logf LOG_LEVEL_NOTICE, "Pressed R. Current Index: %d" 
 
 branchl r12, 0x8023ce38
