a94cb9b4021edb344a6e10ec92c9521305f7f7c9
Charlie Miller <charliea.miller@gmail.com>
2020-08-23 15:29:06 -0800

    Will only allow user to autocomplete if there's already autocompleted text loaded. Otherwise an error sound is played.

--- Online/Menus/CSS/TextEntryScreen/AcceptAutoCompleteOnStartPress.asm
+++ Online/Menus/CSS/TextEntryScreen/AcceptAutoCompleteOnStartPress.asm
@@ -5,6 +5,17 @@
 .include "Common/Common.s"
 
 backup
+
+# Check if we have have an autocomplete result loaded.
+lhz r3, 0x18 (r30)
+cmpwi r3, 0x0
+beq ERROR 
+
+# Play success sound
+li r3, 1
+branchl r12, SFX_Menu_CommonSound
+
+# There's text that can be autocompleted. So we load it.
 li r3, 7 
 stb r3, 0x58 (r28) # store position
 li r0, 57
@@ -16,4 +27,12 @@ sth r3, 0(r4) # Store selection of confirm button
 restore
 
 branchl r12, 0x8023CE4C 
+EXIT:
 branchl r12, 0x8023ce38
+
+ERROR:
+# Play error sound
+li	r3, 3
+branchl r12, SFX_Menu_CommonSound
+restore
+b EXIT
