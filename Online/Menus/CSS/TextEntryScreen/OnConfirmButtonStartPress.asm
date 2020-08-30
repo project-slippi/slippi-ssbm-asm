6d3292064dabc1476f8bfd9699fb3dccf1046668
Charlie Miller <charliea.miller@gmail.com>
2020-08-29 16:20:24 -0800

    Fixed bug where pressing confirm would load non-accepted autcomplete.

--- Online/Menus/CSS/TextEntryScreen/OnConfirmButtonStartPress.asm
+++ Online/Menus/CSS/TextEntryScreen/OnConfirmButtonStartPress.asm
@@ -9,6 +9,22 @@ lbz r3, OFST_R13_NAME_ENTRY_MODE(r13)
 cmpwi r3, 0
 beq EXIT
 
+# Check to see if there's an autcomplete suggestion that hasn't 
+# been accepted.
+lbz r3, 0x58 (r28) # Get cursor position/index.
+cmpwi r3, 7
+bge START
+backup
+li r5, 0
+mulli r3, r3, 2
+CLEAR_REST_TEXT:
+    sthx r5, r30, r3
+    addi r3, r3, 2
+    cmpwi r3, 0xE
+blt CLEAR_REST_TEXT
+restore
+
+START:
 # Play success sound
 li	r3, 1
 branchl r12, SFX_Menu_CommonSound
