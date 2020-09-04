dd4d271ba9405a8542261354d1fb6c177434d303
Charlie Miller <charliea.miller@gmail.com>
2020-09-03 21:22:38 -0800

    Changed how pressing B interacts with autocomplete suggestion.

---
+++ Online/Menus/CSS/TextEntryScreen/OnBPressAutoComplete.asm
@@ -0,0 +1,6 @@
+################################################################################
+# Address: 0x8023cde4 # Jump to 'OnEnterText.asm'
+################################################################################
+
+.include "Common/Common.s"
+branchl r12, 0x8023c730 # Load autocomplete suggestion for text.
