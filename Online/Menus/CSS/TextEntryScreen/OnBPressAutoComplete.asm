3e32160bfa8b7f9cc5a73ea6277ac7d44edd6b3d
Charlie Miller <charliea.miller@gmail.com>
2020-09-03 21:48:09 -0800

    Fixed bug where removing all text would still result in autocomplete suggestion.

--- Online/Menus/CSS/TextEntryScreen/OnBPressAutoComplete.asm
+++ Online/Menus/CSS/TextEntryScreen/OnBPressAutoComplete.asm
@@ -3,4 +3,9 @@
 ################################################################################
 
 .include "Common/Common.s"
+# Don't autocopmlete if we've removed all text.
+cmpwi r0, r5, 1
+beq EXIT
 branchl r12, 0x8023c730 # Load autocomplete suggestion for text.
+EXIT:
+branchl r12, 0x8023ce4c # NameEntry_UpdateTypedName
