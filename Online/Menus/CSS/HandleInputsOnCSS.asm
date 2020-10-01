c04addae36f5fa313a82ce30020c011acd96c52f
Robert Peralta <robert.peralta@helloglobo.com>
2020-10-01 17:30:21 -0400

    Turn off debug

--- Online/Menus/CSS/HandleInputsOnCSS.asm
+++ Online/Menus/CSS/HandleInputsOnCSS.asm
@@ -125,7 +125,7 @@ b SKIP_START_MATCH
 HANDLE_IDLE:
 
 # uncomment to debug the chat window
-bl FN_CHECK_CHAT_INPUTS
+# bl FN_CHECK_CHAT_INPUTS
 
 # When idle, pressing start will start finding match
 # Check if start was pressed
