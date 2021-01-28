704cebe26db8b7560131a762f9504fd844d403f4
Charlie Miller <charliea.miller@gmail.com>
2021-01-27 18:37:28 -0800

    Fixed invalid syntax.

--- Online/Menus/CSS/TextEntryScreen/OnBPress.asm
+++ Online/Menus/CSS/TextEntryScreen/OnBPress.asm
@@ -5,7 +5,7 @@
 .include "Common/Common.s"
 
 # If the cursor is at the last index, we remove the text but don't move the cursor.
-cmpwi r0, r5, 7
+cmpwi r5, 7
 bge LAST_INDEX 
 
 branchl r12, 0x8023cdbc # Move the cursor back one place. 
--- Online/Menus/CSS/TextEntryScreen/OnBPressAutoComplete.asm
+++ Online/Menus/CSS/TextEntryScreen/OnBPressAutoComplete.asm
@@ -4,7 +4,7 @@
 
 .include "Common/Common.s"
 # Don't autocopmlete if we've removed all text.
-cmpwi r0, r5, 1
+cmpwi r5, 1
 beq EXIT
 branchl r12, 0x8023c730 # Load autocomplete suggestion for text.
 EXIT:
