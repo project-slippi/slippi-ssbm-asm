aee9999be73a309d3f2779b844219a0586b240eb
Jas Laferriere <Fizzi36@gmail.com>
2021-03-04 16:22:43 -0800

    add code to apply in-game delay everywhere
    previously delay would only apply to online and practicing was awkward

--- Common/UseInGameDelay/ApplyInGameDelay.asm
+++ Common/UseInGameDelay/ApplyInGameDelay.asm
@@ -25,16 +25,62 @@ bne EXIT
 # Logic Start
 ################################################################################
 .set REG_IGDB_ADDR, 31
+.set REG_CUR_REPORT_IGDB_OFST, 30
+
+.set CONST_BACKUP_BYTES, 0xB0 # Maybe add this to Common.s
+.set P1_PAD_OFFSET, CONST_BACKUP_BYTES + 0x2C
 
 backup
 
 computeBranchTargetAddress INJ_InitInGameDelay
 lwz REG_IGDB_ADDR, 0x8(r3) # Loads the address of the buffer
 
-# I don't think I need to check for zero delay, if someone wants zero delay, they need to turn
-# off the code
+# Check for zero delay, if delay is zero, don't do anything
+lbz r3, IGDB_DELAY_FRAMES(REG_IGDB_ADDR)
+cmpwi r3, 0
+ble RESTORE_EXIT
 
+################################################################################
+# Copy current inputs to temporary location
+################################################################################
+addi r3, sp, BKP_FREE_SPACE_OFFSET
+addi r4, sp, P1_PAD_OFFSET
+li r5, PADS_REPORT_SIZE
+branchl r12, memcpy
 
+################################################################################
+# Overwrite current inputs with inputs from X frames ago
+################################################################################
+# Get IGDB offset for current index
+lbz r3, IGDB_PAD_BUFFER_INDEX(REG_IGDB_ADDR)
+mulli r3, r3, PADS_REPORT_SIZE
+addi REG_CUR_REPORT_IGDB_OFST, r3, IGDB_PAD_BUFFER
+
+# Overwrite
+addi r3, sp, P1_PAD_OFFSET
+add r4, REG_IGDB_ADDR, REG_CUR_REPORT_IGDB_OFST
+li r5, PADS_REPORT_SIZE
+branchl r12, memcpy
+
+################################################################################
+# Store current inputs to use X frames later
+################################################################################
+add r3, REG_IGDB_ADDR, REG_CUR_REPORT_IGDB_OFST
+addi r4, sp, BKP_FREE_SPACE_OFFSET
+li r5, PADS_REPORT_SIZE
+branchl r12, memcpy
+
+################################################################################
+# Increment index
+################################################################################
+lbz r4, IGDB_DELAY_FRAMES(REG_IGDB_ADDR)
+lbz r3, IGDB_PAD_BUFFER_INDEX(REG_IGDB_ADDR)
+addi r3, r3, 1
+cmpw r3, r4
+blt SKIP_WRAP
+li r3, 0 # Wrap to start of buffer once we've delayed enough
+SKIP_WRAP:
+stb r3, IGDB_PAD_BUFFER_INDEX(REG_IGDB_ADDR) # Write new index
 
 RESTORE_EXIT:
 restore
--- Common/UseInGameDelay/InGameDelay.s
+++ Common/UseInGameDelay/InGameDelay.s
@@ -6,7 +6,7 @@
 ################################################################################
 # Constants
 ################################################################################
-.set PAD_REPORT_SIZE, 0xC * 4 # 4 controller inputs to backup
+.set PADS_REPORT_SIZE, 0xC * 4 # 4 controller inputs to backup
 .set MIN_DELAY_FRAMES, 0
 .set MAX_DELAY_FRAMES, 15
 
@@ -15,5 +15,5 @@
 ################################################################################
 .set IGDB_DELAY_FRAMES, 0 # u8
 .set IGDB_PAD_BUFFER_INDEX, IGDB_DELAY_FRAMES + 1 # u8
-.set IGDB_PAD_BUFFER, IGDB_PAD_BUFFER_INDEX + 1 # PAD_REPORT_SIZE * MAX_DELAY_FRAMES
-.set IGDB_SIZE, IGDB_PAD_BUFFER + PAD_REPORT_SIZE * MAX_DELAY_FRAMES
+.set IGDB_PAD_BUFFER, IGDB_PAD_BUFFER_INDEX + 1 # PADS_REPORT_SIZE * MAX_DELAY_FRAMES
+.set IGDB_SIZE, IGDB_PAD_BUFFER + PADS_REPORT_SIZE * MAX_DELAY_FRAMES
