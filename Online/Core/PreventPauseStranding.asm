3b4b050e24e9250f622512a8414c64ac54accde9
Robert Peralta <rapito@gmail.com>
2021-04-20 22:02:29 -0400

    Allow to pause again by tracking down if we already unpaused on disconnect - Without this it makes it very difficult to LRAS because you are automatically unpaused

--- Online/Core/PreventPauseStranding.asm
+++ Online/Core/PreventPauseStranding.asm
@@ -6,6 +6,9 @@
 .include "Common/Common.s"
 .include "Online/Online.s"
 
+# offset from match info block which we can borrow for this
+.set OFST_UNPAUSED_ON_DISCONNECT, 0x9 # can also use 0xb and 0xc
+
 backup
 
 # Ensure that this is an online match
@@ -18,7 +21,15 @@ lbz r3, ODB_IS_DISCONNECTED(r3)
 cmpwi r3, 1
 bne EXIT_AND_RESTORE # if we are not disconnected, just continue as normal
 
+# make sure we have not already unpaused on disconnect once already
+lbz	r3, OFST_UNPAUSED_ON_DISCONNECT(r30)
+cmpwi r3, 1
+beq EXIT_AND_RESTORE
+
 # if we are disconnected, just branch to unpause
+# and store that we already unpaused on disconnect
+li r3, 1
+stb	r3, OFST_UNPAUSED_ON_DISCONNECT(r30)
 restore
 branch r12, 0x8016cd28
 
