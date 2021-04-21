1b5df81fc10d0bef2b27a947e614debac22e80f3
Robert Peralta <rapito@gmail.com>
2021-04-20 22:39:49 -0400

    Remove unnecessary backups/restore - use r3 as much as possible (stop using r12) - renamte label to EXIT

--- Online/Core/PreventPauseStranding.asm
+++ Online/Core/PreventPauseStranding.asm
@@ -9,30 +9,26 @@
 # offset from match info block which we can borrow for this
 .set OFST_UNPAUSED_ON_DISCONNECT, 0x9 # can also use 0xb and 0xc
 
-backup
-
 # Ensure that this is an online match
 getMinorMajor r3
 cmpwi r3, SCENE_ONLINE_IN_GAME
-bne EXIT_AND_RESTORE
+bne EXIT
 
 lwz r3, OFST_R13_ODB_ADDR(r13) # data buffer address
 lbz r3, ODB_IS_DISCONNECTED(r3)
 cmpwi r3, 1
-bne EXIT_AND_RESTORE # if we are not disconnected, just continue as normal
+bne EXIT # if we are not disconnected, just continue as normal
 
 # make sure we have not already unpaused on disconnect once already
 lbz	r3, OFST_UNPAUSED_ON_DISCONNECT(r30)
 cmpwi r3, 1
-beq EXIT_AND_RESTORE
+beq EXIT
 
 # if we are disconnected, just branch to unpause
 # and store that we already unpaused on disconnect
 li r3, 1
 stb	r3, OFST_UNPAUSED_ON_DISCONNECT(r30)
-restore
-branch r12, 0x8016cd28
+branch r3, 0x8016cd28
 
-EXIT_AND_RESTORE:
-restore
+EXIT:
 addi r29, r4, 0 # original line
