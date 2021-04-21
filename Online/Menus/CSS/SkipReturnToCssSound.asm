a17a2fe6d024e2265b0cd93a5d70f3d90882c80a
Jas Laferriere <Fizzi36@gmail.com>
2021-04-21 04:05:16 -0700

    better SSS->CSS sound skip logic

--- Online/Menus/CSS/SkipReturnToCssSound.asm
+++ Online/Menus/CSS/SkipReturnToCssSound.asm
@@ -7,7 +7,9 @@
 .include "Online/Menus/CSS/TextEntryScreen/AutoComplete.s"
 
 .set REG_ACB_ADDR, 31
-.set REG_CONN_STATE, 30
+.set REG_LOCK_IN_STATE, 30
+
+# This logic only runs in 1P mode CSS, so no need to worry about VS mode compatibility
 
 backupall # r3 needs to be restored for replaced codeline
 
@@ -15,17 +17,15 @@ lbz r3, OFST_R13_NAME_ENTRY_MODE(r13)
 cmpwi r3, 0
 bne RESET_NAME_ENTRY
 
-# The following logic handles the transition coming back from SSS
+# The following logic handles the transition coming back from SSS. If we are locked-in, that means
+# that the stage was selected, "Choose your character" would then be an incorrect prompt, in that
+# situation, we skip playing the sound. If coming back from SSS though, sound will play
 li r3, 0
 branchl r12, FN_LoadMatchState
-lbz REG_CONN_STATE, MSRB_CONNECTION_STATE(r3)
+lbz REG_LOCK_IN_STATE, MSRB_IS_LOCAL_PLAYER_READY(r3)
 branchl r12, HSD_Free
-cmpwi REG_CONN_STATE, MM_STATE_CONNECTION_SUCCESS
-bne EXIT # If not connected, don't skip
-
-lbz r3, OFST_R13_ISWINNER(r13)
-cmpwi r3, ISWINNER_LOST
-beq SKIP_SOUND # If not previous loser,
+cmpwi REG_LOCK_IN_STATE, 0
+bne SKIP_SOUND # If locked in, skip sound
 
 b EXIT
 
