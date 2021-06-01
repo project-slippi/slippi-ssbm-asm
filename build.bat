4be6c61779f6b80fc01cd7d1e8551a8a9f5c4c3e
Jas Laferriere <Fizzi36@gmail.com>
2021-06-01 02:59:24 -0700

    add code for 4:3 aspect ratio on monitor

--- build.bat
+++ build.bat
@@ -25,6 +25,7 @@ set list=%list%;console_frozen.json
 set list=%list%;console_lag_pd.json
 set list=%list%;console_lag_pdvb.json
 set list=%list%;console_screen_wide.json
+set list=%list%;console_screen_mon43.json
 
 for %%a in (%list%) do (
   echo Building %%a...
