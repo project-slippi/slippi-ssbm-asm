1a583909d2619873d727d78435a26cd3745d5bb7
UnclePunch <vinbertinelli@gmail.com>
2021-08-05 08:08:39 -0400

    add alternate stage name display (not applicable to vanilla)
    adds hook to set the stage name on the intro (VS) screen

--- Common/Common.s
+++ Common/Common.s
@@ -169,6 +169,7 @@ add \reg, r3, r4
 .set FN_GetTeamCostumeIndex,0x800056b0
 .set FN_GetCSSIconData,0x800056b8
 .set FN_AdjustNullID,0x80005694
+.set FN_CheckAltStageName,0x80005690
 .set FN_GetCSSIconNum,0x80005698
 .set FN_LoadPremadeText, 0x800056a4
 .set FN_GetSSMIndex,0x800056a0
@@ -177,7 +178,7 @@ add \reg, r3, r4
 .set FN_RequestSSM,0x800056a8
 .set FN_GetCommonMinorID,0x8000561c
 # available addresses for static functions
-# 0x80005690
+# 0x8000568C
 
 # Online static functions
 .set FN_CaptureSavestate,0x80005608
