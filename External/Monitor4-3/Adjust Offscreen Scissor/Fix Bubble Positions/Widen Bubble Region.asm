2b36a64a8cdec4d5d3218d20f575370e1be70aa1
Jas Laferriere <Fizzi36@gmail.com>
2021-10-06 18:51:58 -0400

    change mon43 code to go from 16:9 -> 73:60

--- External/Monitor4-3/Adjust Offscreen Scissor/Fix Bubble Positions/Widen Bubble Region.asm
+++
@@ -1,6 +0,0 @@
-################################################################################
-# Address: 804ddb4c
-################################################################################
-
-# As far as I can tell, this is the only value in this folder that makes any difference
-.float 0.0913 # Original: 0.1
