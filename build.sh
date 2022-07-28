fed9c8a3dd6c49a1ad79fdab5ac323bffaf5f47b
Ownasaurus <Ownasaurus@gmail.com>
2022-07-27 21:10:44 -0400

    update build.sh to match build.bat (#96)

--- build.sh
+++ build.sh
@@ -3,25 +3,31 @@ source ./build-netplay.sh
 
 source ./build-playback.sh
 
-echo Building console_core.json...
-gecko build -c console_core.json -defsym "STG_EXIIndex=1" -o "Output/Console/g_core.bin"
-echo ""
-
 echo Building console_core.json for Port A...
 gecko build -c console_core.json -defsym "STG_EXIIndex=0" -o "Output/Console/g_core_porta.bin"
+echo ""
+
+echo Building console_core.json...
+gecko build -c console_core.json -defsym "STG_EXIIndex=1" -o "Output/Console/g_core.bin"
 echo ""
 
 list=(
     "console_UCF.json"
     "console_UCF_stealth.json"
-    "console_ControllerFixPlayerToggles.json"
     "console_mods_stealth.json"
     "console_mods_tournament.json"
     "console_mods_friendlies.json"
     "console_PAL.json"
-    "console_frozen.json"
+    "console_stages_stadium.json"
+    "console_stages_all.json"
+    "console_gameplay_lgl.json"
+    "console_gameplay_wobbling.json"
+    "console_gameplay_both.json"
     "console_lag_pd.json"
     "console_lag_pdhalfvb.json"
+    "console_screen_wide.json"
+    "console_screen_wide_shutters.json"
+    "console_safety.json"
 )
 
 for file in "${list[@]}"
