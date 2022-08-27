35625a6166c07913fed1ef1be0bd050802d750d5
Jas Laferriere <Fizzi36@gmail.com>
2022-08-27 02:21:55 -0400

    add float backup/restore macro
    fix bug where non-volatile floating point registers were not being restored

--- Common/Common.s
+++ Common/Common.s
@@ -95,6 +95,143 @@ lmw r20,0x8(r1)
 mtlr r0
 .endm
 
+.set F_BKP_FREE_SPACE_OFFSET, 0xE0 # This is where the free space in our stack starts
+
+# fbackup can be used to backup/restore float registers and also does normal registers from r14
+# instead of normal backup for which registers 14-19 are unsafe to use
+.macro fbackup num_freg=0, free_space=0x10
+mflr r0
+stw r0, 0x4(r1)
+# Stack allocation has to be 4-byte aligned otherwise it crashes on console
+.if \free_space % 4 == 0
+  stwu r1,-(F_BKP_FREE_SPACE_OFFSET + \free_space)(r1)	# make space for 12 registers
+.else
+  stwu r1,-(F_BKP_FREE_SPACE_OFFSET + \free_space + (4 - \free_space % 4))(r1)	# make space for 12 registers
+.endif
+stmw r14,0x8(r1)
+.if \num_freg > 0
+  stfd f31, 0x50(r1)
+.endif
+.if \num_freg > 1
+  stfd f30, 0x58(r1)
+.endif
+.if \num_freg > 2
+  stfd f29, 0x60(r1)
+.endif
+.if \num_freg > 3
+  stfd f28, 0x68(r1)
+.endif
+.if \num_freg > 4
+  stfd f27, 0x70(r1)
+.endif
+.if \num_freg > 5
+  stfd f26, 0x78(r1)
+.endif
+.if \num_freg > 6
+  stfd f25, 0x80(r1)
+.endif
+.if \num_freg > 7
+  stfd f24, 0x88(r1)
+.endif
+.if \num_freg > 8
+  stfd f23, 0x90(r1)
+.endif
+.if \num_freg > 9
+  stfd f22, 0x98(r1)
+.endif
+.if \num_freg > 10
+  stfd f21, 0xA0(r1)
+.endif
+.if \num_freg > 11
+  stfd f20, 0xA8(r1)
+.endif
+.if \num_freg > 12
+  stfd f19, 0xB0(r1)
+.endif
+.if \num_freg > 13
+  stfd f18, 0xB8(r1)
+.endif
+.if \num_freg > 14
+  stfd f17, 0xC0(r1)
+.endif
+.if \num_freg > 15
+  stfd f16, 0xC8(r1)
+.endif
+.if \num_freg > 16
+  stfd f15, 0xD0(r1)
+.endif
+.if \num_freg > 17
+  stfd f14, 0xD8(r1)
+.endif
+.endm
+
+.macro frestore num_freg=0, free_space=0x10
+lmw r14,0x8(r1)
+.if \num_freg > 0
+  lfd f31, 0x50(r1)
+.endif
+.if \num_freg > 1
+  lfd f30, 0x58(r1)
+.endif
+.if \num_freg > 2
+  lfd f29, 0x60(r1)
+.endif
+.if \num_freg > 3
+  lfd f28, 0x68(r1)
+.endif
+.if \num_freg > 4
+  lfd f27, 0x70(r1)
+.endif
+.if \num_freg > 5
+  lfd f26, 0x78(r1)
+.endif
+.if \num_freg > 6
+  lfd f25, 0x80(r1)
+.endif
+.if \num_freg > 7
+  lfd f24, 0x88(r1)
+.endif
+.if \num_freg > 8
+  lfd f23, 0x90(r1)
+.endif
+.if \num_freg > 9
+  lfd f22, 0x98(r1)
+.endif
+.if \num_freg > 10
+  lfd f21, 0xA0(r1)
+.endif
+.if \num_freg > 11
+  lfd f20, 0xA8(r1)
+.endif
+.if \num_freg > 12
+  lfd f19, 0xB0(r1)
+.endif
+.if \num_freg > 13
+  lfd f18, 0xB8(r1)
+.endif
+.if \num_freg > 14
+  lfd f17, 0xC0(r1)
+.endif
+.if \num_freg > 15
+  lfd f16, 0xC8(r1)
+.endif
+.if \num_freg > 16
+  lfd f15, 0xD0(r1)
+.endif
+.if \num_freg > 17
+  lfd f14, 0xD8(r1)
+.endif
+# Stack allocation has to be 4-byte aligned otherwise it crashes on console
+.if \free_space % 4 == 0
+  lwz r0, (F_BKP_FREE_SPACE_OFFSET + 0x4 + \free_space)(r1)
+  addi r1,r1,F_BKP_FREE_SPACE_OFFSET + \free_space	# release the space
+.else
+  lwz r0, (F_BKP_FREE_SPACE_OFFSET + 0x4 + \free_space + (4 - \free_space % 4))(r1)
+  addi r1,r1,F_BKP_FREE_SPACE_OFFSET + \free_space + (4 - \free_space % 4)	# release the space
+.endif
+mtlr r0
+.endm
+
 .macro byteAlign32 reg
 addi \reg, \reg, 31
 rlwinm \reg, \reg, 0, 0xFFFFFFE0
