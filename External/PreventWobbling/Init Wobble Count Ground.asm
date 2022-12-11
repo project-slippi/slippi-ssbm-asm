1c93be8e0ba7e4306d4010a39d4d58b084ceb333
Jas Laferriere <Fizzi36@gmail.com>
2022-12-11 16:02:44 -0500

    enforce no wobbling via code
    this is currently applied across the board. will try to address this in the future to only enable it in ranked perhaps

--- External/PreventWobbling/Init Wobble Count Ground.asm
+++ External/PreventWobbling/Init Wobble Count Ground.asm
@@ -2,19 +2,9 @@
 # Address: 800dbbd4
 # Tags: [affects-gameplay]
 ################################################################################
-.include "Common/Common.s"
+.include "External/PreventWobbling/PreventWobbling.s"
 
-.set  REG_DefenderData,29
+Wobbling_InitWobbleCount
 
-.set OFST_WobbleCounter,0x2384
-.set OFST_LastMoveID,0x2386
-
-#Init count
-  li  r3,0
-  stb r3,OFST_WobbleCounter(REG_DefenderData)
-#Init last move ID
-  li  r3,-1
-  sth r3,OFST_LastMoveID(REG_DefenderData)
-  
 #Original codeline
-  lwz	r0, 0x005C (sp)
+lwz	r0, 0x005C (sp)
---
+++ External/PreventWobbling/Ranked/Init Wobble Count Ground.asm
@@ -0,0 +1,27 @@
+################################################################################
+# Address: 800dbbd4
+# Tags: [affects-gameplay]
+################################################################################
+.include "External/PreventWobbling/PreventWobbling.s"
+.include "Online/Online.s"
+.include "Common/Common.s"
+
+# TODO: This doesn't actually work. It would be enabled for all online games when playing but
+# TODO: if you went to VS mode to play a local game, it wouldn't be enabled when playing but would
+# TODO: be enabled when that replay was played. We will have to pass the scene and mode the game
+# TODO: was played to the playback engine to properly decide when to enable this code in a replay
+
+# Only run this code in ranked and playback
+getMinorMajor r3
+cmpwi r3, SCENE_ONLINE_IN_GAME
+beq EXEC_CODE
+cmpwi r3, SCENE_PLAYBACK_IN_GAME
+beq EXEC_CODE
+b WRAPPER_EXIT
+
+EXEC_CODE:
+Wobbling_InitWobbleCount
+
+WRAPPER_EXIT:
+#Original codeline
+lwz	r0, 0x005C (sp)
