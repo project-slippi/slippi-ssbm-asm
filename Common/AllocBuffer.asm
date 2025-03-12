94935362163fa21cfca8e2e681f2e798cf50e90f
KELLZ <forestruns@gmail.com>
2025-03-12 15:11:47 -0230

    fix bug where certain gfx textures would appear all white

--- Common/AllocBuffer.asm
+++ Common/AllocBuffer.asm
@@ -5,6 +5,10 @@
 .include "Common/Common.s"
 .include "Online/Online.s"
 
+# Free old buffer
+  lwz r3,OFST_R13_SB_ADDR(r13)
+  branchl r12, HSD_Free
+
 # Alloc buffer
   li  r3,128
   branchl r12,HSD_MemAlloc
