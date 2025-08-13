acc11ae502c44f4589c56739408333a39870fbe6
Jas Laferriere <Fizzi36@gmail.com>
2025-08-13 01:21:44 -0400

    make fd transformations not affect rng seed

---
+++ External/FreezeFDSlippi/FreezeFDSlippi.asm
@@ -0,0 +1,9 @@
+################################################################################
+# Address: 0x8021aae0
+# Tags: [affects-gameplay]
+################################################################################
+# This is a slightly different freeze implementation so that the RNG protection
+# unfrozen FD code can have its own injection.
+################################################################################
+
+b 0x8
