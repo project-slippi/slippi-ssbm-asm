1dde16f4311de0ee955d084c8fdc3fc756a328cd
charlotte <charlottemackenzi@gmail.com>
2025-11-29 14:07:12 +1300

    feat: 16:10 widescreen

---
+++ External/16.10 Widescreen/Overwrite CObj Values.asm
@@ -0,0 +1,26 @@
+################################################################################
+# Address: 8036A4A8
+################################################################################
+.include "../../Common/Common.s"
+
+# initialize widescreen
+li r3, 0 # we dont set it to make it actually widescreen here as various C places cut off chat messages and chat commands if we do. the 4:3 values for those are better for 16:10
+stb r3, isWidescreen(r13)
+
+lfs	f1,0x34(r31)	# default code line
+
+bl  Floats
+mflr r3
+lfs f2,0x0(r3)
+lfs f3,0x4(r3)
+fmuls f1,f1,f2
+fdivs f1,f1,f3
+
+b END
+
+Floats:
+blrl
+.float 320
+.float 243 # adjusted for 16:10 (219 * (16/10) / (16/9))
+
+END:
